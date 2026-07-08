3b7e79ab87f3707f20a17118e59ad6aab9a72d2a
Akhila <akhila@Akhilas-MacBook-Air-2.local>
2026-07-08 21:53:15 +0530

    Updated Day 3 classwork files with complete source code

--- Day3_JAVA/Classwork/StudentApp.java
+++ Day3_JAVA/Classwork/StudentApp.java
@@ -2,4 +2,11 @@ package com.coforge.classwork.day3;
 
 public class StudentApp {
 
+	public static void main(String[] args) {
+		
+		Student akhila = new Student(101,"akhila","akhila@gmail.com");
+		System.out.println(akhila);
+
+	}
+
 }
